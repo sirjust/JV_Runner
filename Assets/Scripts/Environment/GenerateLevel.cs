fd8c5f1d252aa89b6ec224fb92aa733b6c114b29
Justin Moore <theoperaking@yahoo.com>
2023-07-28 11:37:51 -0700

    Add infinite section generation

---
+++ Assets/Scripts/Environment/GenerateLevel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public GameObject[] sections;
+    public int zPos = 50;
+    public bool creatingSection = false;
+    public int secNum;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!creatingSection)
+        {
+            creatingSection = true;
+            StartCoroutine(GenerateSection());
+        }
+    }
+
+    private IEnumerator GenerateSection()
+    {
+        secNum = Random.Range(0, 3);
+        Instantiate(sections[secNum], new Vector3(0f, 0f, zPos), Quaternion.identity);
+        zPos += 50;
+        yield return new WaitForSeconds(2);
+        creatingSection = false;
+    }
+}
