25b5f377ccbbcd6f1dfc63799ea2f9b285087d0f
Justin Moore <theoperaking@yahoo.com>
2023-08-01 16:55:02 -0700

    Add UI to display number of coins

--- Assets/Scripts/Collectables/CollectCoin.cs
+++ Assets/Scripts/Collectables/CollectCoin.cs
@@ -9,6 +9,7 @@ public class CollectCoin : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         coinFX.Play();
+        CollectableControl.coinCount++;
         this.gameObject.SetActive(false);
     }
 }
