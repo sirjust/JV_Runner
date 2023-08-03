307801e9f47f153f2f3014d46a263aeb0dc033db
Justin Moore <theoperaking@yahoo.com>
2023-08-03 07:46:00 -0700

    Add sfx and camera shake on collision, add enemy

--- Assets/Scripts/Environment/ObstacleCollision.cs
+++ Assets/Scripts/Environment/ObstacleCollision.cs
@@ -6,11 +6,15 @@ public class ObstacleCollision : MonoBehaviour
 {
     public GameObject player;
     public GameObject charModel;
+    public AudioSource crashThud;
+    public GameObject mainCam;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         player.GetComponent<PlayerMove>().enabled = false;
         charModel.GetComponent<Animator>().Play("Stumble Backwards");
+        crashThud.Play();
+        mainCam.GetComponent<Animator>().enabled = true;
     }
 }
