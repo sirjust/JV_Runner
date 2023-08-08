48997be33e1df7bd73a6ae40cbba565c1bdba6c4
Justin Moore <theoperaking@yahoo.com>
2023-08-08 11:53:13 -0700

    Enable jumping

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -7,6 +7,9 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed = 5f;
     public float leftRightSpeed = 4f;
     public static bool canMove = false;
+    public bool isJumping = false;
+    public bool comingDown = false;
+    public GameObject playerObject;
 
     void Update()
     {
@@ -29,6 +32,40 @@ public class PlayerMove : MonoBehaviour
                     transform.Translate(Vector3.left * Time.deltaTime * -leftRightSpeed, Space.World);
                 }
             }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+            {
+                if (!isJumping)
+                {
+                    isJumping = true;
+                    playerObject.GetComponent<Animator>().Play("Jump");
+                    StartCoroutine(JumpSequence());
+                }
+            }
         }
+
+        if (isJumping)
+        {
+            if (!comingDown)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 3f, Space.World);
+            }
+            else
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * -3f, Space.World);
+            }
+        }
+    }
+
+    IEnumerator JumpSequence()
+    {
+        float initialHeight = transform.position.y;
+        yield return new WaitForSeconds(0.45f);
+        comingDown = true;
+        yield return new WaitForSeconds(0.45f);
+        isJumping = false;
+        comingDown = false;
+        playerObject.GetComponent<Animator>().Play("Running");
+        transform.position = new Vector3(transform.position.x, initialHeight, transform.position.z);
     }
 }
